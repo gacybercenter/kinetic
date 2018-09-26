3a92faf420c006d3bb2f3ce17fbb7569a6e06439
bitskrieg <bitskrieg@bitskrieg.net>
2018-09-26 09:38:59 -0400

    update bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -24,16 +24,12 @@ DEBIAN_FRONTEND=noninteractive
 ## Packages
 apt-get update
 apt-get -y dist-upgrade
-apt-get -y install qemu-kvm qemu-utils genisoimage bridge-utils
-
-## Networking
-
-address=$(hostname -I)
+apt-get -y install qemu-kvm qemu-utils genisoimage curl
 
 mkdir -p /kvm/images
 mkdir -p /kvm/vms/salt
 mkdir /kvm/vms/dnsmasq
 
 wget https://cdimage.debian.org/cdimage/openstack/current-9/debian-9-openstack-amd64.raw -O /kvm/images/debian9.raw
-curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed 's/{{ name }}/salt/g' > /kvm/vms/salt/config.xml
-curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed 's/{{ name }}/dnsmasq/g' > /kvm/vms/dnsmasq/config.xml
+curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed 's/{{ name }}/salt/g; s/{{ interface }}/$interface/g' > /kvm/vms/salt/config.xml
+curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed 's/{{ name }}/dnsmasq/g; s/{{ interface }}/$interface/g' > /kvm/vms/dnsmasq/config.xml
