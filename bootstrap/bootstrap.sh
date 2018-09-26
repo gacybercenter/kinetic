2fe3dc6a5a967b70b6e56fe5dd47fd19fceab9e0
bitskrieg <bitskrieg@bitskrieg.net>
2018-09-26 09:50:32 -0400

    fix shell expansion

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -31,5 +31,5 @@ mkdir -p /kvm/vms/salt
 mkdir /kvm/vms/dnsmasq
 
 wget https://cdimage.debian.org/cdimage/openstack/current-9/debian-9-openstack-amd64.raw -O /kvm/images/debian9.raw
-curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed 's/{{ name }}/salt/g; s/{{ interface }}/$interface/g' > /kvm/vms/salt/config.xml
-curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed 's/{{ name }}/dnsmasq/g; s/{{ interface }}/$interface/g' > /kvm/vms/dnsmasq/config.xml
+curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed "s/{{ name }}/salt/g; s/{{ interface }}/$interface/g" > /kvm/vms/salt/config.xml
+curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed "s/{{ name }}/dnsmasq/g; s/{{ interface }}/$interface/g" > /kvm/vms/dnsmasq/config.xml
