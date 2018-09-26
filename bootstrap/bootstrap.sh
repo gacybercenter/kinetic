f431def39cc769677c8d422c56c985fb9f0e5752
bitskrieg <bitskrieg@bitskrieg.net>
2018-09-26 08:34:46 -0400

    update bootstrap.sh to pull down customized config.xml for salt and dnsmasq

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -11,7 +11,9 @@ apt-get update
 apt-get -y dist-upgrade
 apt-get -y install qemu-kvm qemu-utils genisoimage
 mkdir -p /kvm/images
+mkdir -p /kvm/vms/salt
+mkdir /kvm/vms/dnsmasq
 
 wget https://cdimage.debian.org/cdimage/openstack/current-9/debian-9-openstack-amd64.raw -O /kvm/images/debian9.raw
-curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml
-
+curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed 's/{{ name }}/salt/g' > /kvm/vms/salt/config.xml
+curl https://raw.githubusercontent.com/GeorgiaCyber/kinetic/master/bootstrap/resources/common.xml | sed 's/{{ name }}/dnsmasq/g' > /kvm/vms/dnsmasq/config.xml
