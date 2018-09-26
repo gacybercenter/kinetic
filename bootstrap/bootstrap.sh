d41bce69742f0cd253fce82a6fa53066c765dc10
bitskrieg <bitskrieg@bitskrieg.net>
2018-09-26 16:08:32 -0400

    Revert "remove arguments"
    This reverts commit 71167f235e73fd0280fb9f88ca729d9f797ba8c4.

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,6 +5,27 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  exit 2
+fi
+
+while getopts ":i:" opt; do
+  case ${opt} in
+    i )
+      interface=$OPTARG
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG.  The only valid option is -i, which specifies the bridged management interface on this device" 1>&2
+      exit
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      exit
+      ;;
+  esac
+done
+
 DEBIAN_FRONTEND=noninteractive
 
 ## Packages
