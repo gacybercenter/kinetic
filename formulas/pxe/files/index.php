427ba357ac4b45c6b54b1f6b4ed0f6834d1242fd
bitskrieg <bitskrieg@bitskrieg.net>
2018-09-30 03:12:26 -0400

    update bootfiles

--- formulas/pxe/files/index.php
+++ formulas/pxe/files/index.php
@@ -1,6 +1,9 @@
 <?php
+header('Content-type: text/plain');
 $mac = ($_GET["mac"]);
 exec("grep $mac hosts", $output);
-$type_array = explode("=", $output[0]);
+$type_array = explode(" = ", $output[0]);
 $type = $type_array[1];
+$bootfile = file_get_contents("$type.pxe");
+echo $bootfile;
 ?>
