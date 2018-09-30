11f58e53a2f19b45d63af7761aad4da58fb73e50
bitskrieg <bitskrieg@bitskrieg.net>
2018-09-30 14:33:34 -0400

    randomize hostname generation

--- formulas/pxe/files/index.php
+++ formulas/pxe/files/index.php
@@ -4,6 +4,9 @@ $mac = ($_GET["mac"]);
 exec("grep $mac hosts", $output);
 $type_array = explode(" = ", $output[0]);
 $type = $type_array[1];
+exec("echo -n $type-$(uuidgen)", $hostname);
 $bootfile = file_get_contents("$type.pxe");
-echo str_replace("http://pxe", "http://".$_SERVER['SERVER_ADDR'], $bootfile);
+$bootfile = str_replace("http://pxe", "http://".$_SERVER['SERVER_ADDR'], $bootfile);
+$bootfile = str_replace("undefined-hostname", "$hostname[0]", $bootfile);
+echo $bootfile;
 ?>
