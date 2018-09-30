69f90e3cec6dd1e47b08cc51eea60be7cb4f2b80
bitskrieg <bitskrieg@bitskrieg.net>
2018-09-30 18:55:58 -0400

    testing common pxe file

--- formulas/pxe/files/index.php
+++ formulas/pxe/files/index.php
@@ -6,7 +6,7 @@ $type_array = explode(" = ", $output[0]);
 $type = $type_array[1];
 exec("echo -n $type-$(uuidgen)", $hostname);
 $bootfile = file_get_contents("common.pxe");
-$bootfile = str_replace("http://pxe/preseed/host-type.preseed", "http://".$_SERVER['SERVER_ADDR']./"preseed/".$type.".preseed", $bootfile);
+$bootfile = str_replace("http://pxe/preseed/host-type.preseed", "http://".$_SERVER['SERVER_ADDR']."/preseed/".$type.".preseed", $bootfile);
 $bootfile = str_replace("undefined-hostname", "$hostname[0]", $bootfile);
 echo $bootfile;
 ?>
