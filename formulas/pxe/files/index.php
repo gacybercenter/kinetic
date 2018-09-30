7519db6efea5c00859dbeeb5b066ca42c48b38fe
bitskrieg <bitskrieg@bitskrieg.net>
2018-09-30 18:52:01 -0400

    testing common pxe file

--- formulas/pxe/files/index.php
+++ formulas/pxe/files/index.php
@@ -5,8 +5,8 @@ exec("grep $mac hosts", $output);
 $type_array = explode(" = ", $output[0]);
 $type = $type_array[1];
 exec("echo -n $type-$(uuidgen)", $hostname);
-$bootfile = file_get_contents("$type.pxe");
-$bootfile = str_replace("http://pxe", "http://".$_SERVER['SERVER_ADDR'], $bootfile);
+$bootfile = file_get_contents("common.pxe");
+$bootfile = str_replace("http://pxe/preseed/host-type.preseed", "http://".$_SERVER['SERVER_ADDR']./"preseed/".$type.".preseed", $bootfile);
 $bootfile = str_replace("undefined-hostname", "$hostname[0]", $bootfile);
 echo $bootfile;
 ?>
