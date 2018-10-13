3ab47a4f233b43c2a672b17081e888f950eb6b9d
bitskrieg <bitskrieg@bitskrieg.net>
2018-10-13 18:34:02 -0400

    update index

--- formulas/pxe/files/index.php
+++ formulas/pxe/files/index.php
@@ -8,5 +8,6 @@ exec("echo -n $type-$(uuidgen)", $hostname);
 $bootfile = file_get_contents("common.pxe");
 $bootfile = str_replace("http://pxe/preseed/host-type.preseed", "http://".$_SERVER['SERVER_ADDR']."/preseed/".$type.".preseed", $bootfile);
 $bootfile = str_replace("undefined-hostname", "$hostname[0]", $bootfile);
+file_put_contents("pending_hosts/$hostname[0]","$hostname[0]");
 echo $bootfile;
 ?>
