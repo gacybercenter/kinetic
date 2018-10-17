92f6d6ef02798f1cb28a23cdb146d98068225fa3
bitskrieg <bitskrieg@bitskrieg.net>
2018-10-17 18:35:08 -0400

    testing

--- _modules/netcalc.py
+++ _modules/netcalc.py
@@ -1,7 +1,30 @@
-import ipaddress
+import salt.utils.network as network
 
-def ip_addrs(cidr=None):
-    ip = list(ipaddress.ip_network(cidr).hosts())
-    return [i for i in ip]
+__virtualname__ = 'netcalc'
+
+def __virtual__():
+    return __virtualname__
+
+def gethosts(cidr):
+	return network._network_hosts(cidr)
+
+
+#def network_hosts(ip_addr_entry):
+#    return [
+#        six.text_type(host)
+#        for host in ipaddress.ip_network(ip_addr_entry, strict=False).hosts()
+#    ]
+
+#def network_hosts(cidr):
+#	    return [
+#        six.text_type(host)
+#        for host in ipaddress.ip_network(cidr, strict=False).hosts()
+#    ]
+
+#print network_hosts('192.168.200.0/24')
+
+#def ip_addrs(cidr=None):
+#    ip = list(ipaddress.ip_network(cidr).hosts())
+#    return [i for i in ip]
 
 #print ip_addrs(u'192.168.0.0/24')
