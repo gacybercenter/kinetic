471d81981af54f504a344fd90f067eb8aa9ea5f7
bitskrieg <bitskrieg@bitskrieg.net>
2018-10-17 18:37:49 -0400

    finalize custom module

--- _modules/netcalc.py
+++ _modules/netcalc.py
@@ -7,24 +7,3 @@ def __virtual__():
 
 def gethosts(cidr):
 	return network._network_hosts(cidr)
-
-
-#def network_hosts(ip_addr_entry):
-#    return [
-#        six.text_type(host)
-#        for host in ipaddress.ip_network(ip_addr_entry, strict=False).hosts()
-#    ]
-
-#def network_hosts(cidr):
-#	    return [
-#        six.text_type(host)
-#        for host in ipaddress.ip_network(cidr, strict=False).hosts()
-#    ]
-
-#print network_hosts('192.168.200.0/24')
-
-#def ip_addrs(cidr=None):
-#    ip = list(ipaddress.ip_network(cidr).hosts())
-#    return [i for i in ip]
-
-#print ip_addrs(u'192.168.0.0/24')
