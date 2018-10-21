d78679b2afa2b85df6ef5fd3944c6e24df6fa6bc
bitskrieg <bitskrieg@bitskrieg.net>
2018-10-21 16:21:57 -0400

    implementing super hack to work around salt 34581.  Kill this with fire ASAP

--- _modules/minionmanage.py
+++ _modules/minionmanage.py
@@ -6,7 +6,31 @@ __virtualname__ = 'minionmanage'
 def __virtual__():
     return __virtualname__
 
-def populate(path):
+def populate_cache(path):
+	pending = file.readdir(path)
+	pending.remove('.')
+	pending.remove('..')
+	return pending
+
+def populate_contoller(path):
+	pending = file.readdir(path)
+	pending.remove('.')
+	pending.remove('..')
+	return pending
+
+def populate_controllerv2(path):
+	pending = file.readdir(path)
+	pending.remove('.')
+	pending.remove('..')
+	return pending
+
+def populate_storage(path):
+	pending = file.readdir(path)
+	pending.remove('.')
+	pending.remove('..')
+	return pending
+
+def populate_compute(path):
 	pending = file.readdir(path)
 	pending.remove('.')
 	pending.remove('..')
