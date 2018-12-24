fb8ead4ea570f6ee2d1226c1372e823c21895fe4
bitskrieg <bitskrieg@bitskrieg.net>
2018-12-24 17:06:52 -0500

    update index.php to include interface selection

--- formulas/pxe/files/index.php
+++ formulas/pxe/files/index.php
@@ -5,9 +5,11 @@ exec("grep $mac hosts", $output);
 $type_array = explode(" = ", $output[0]);
 $type = $type_array[1];
 exec("echo -n $type-$(uuidgen)", $hostname);
+exec("grep 'd-i netcfg/choose_interface' preseed/$type.preseed | awk '{ print $4 }'", $interface);
 $bootfile = file_get_contents("common.pxe");
 $bootfile = str_replace("http://pxe/preseed/host-type.preseed", "http://".$_SERVER['SERVER_ADDR']."/preseed/".$type.".preseed", $bootfile);
 $bootfile = str_replace("undefined-hostname", "$hostname[0]", $bootfile);
+$bootfile = str_replace("interface=auto", "interface=$interface[0]", $bootfile);
 if (!is_dir('pending_hosts/' . $type)) {
   mkdir('pending_hosts/' . $type);
 }
