0cea48189857393791954b1fa29c11ce6f91aa80
bitskrieg <bitskrieg@bitskrieg.net>
2018-12-30 18:09:30 -0500

    streamline initialize

--- formulas/keystone/files/initialize.sh
+++ formulas/keystone/files/initialize.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+export OS_USERNAME=admin
+export OS_PASSWORD={{ admin_password }}
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL={{ internal_endpoint }}
+export OS_IDENTITY_API_VERSION=3
+
 
 /bin/sh -c "keystone-manage db_sync" keystone
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
@@ -10,107 +18,38 @@ keystone-manage bootstrap --bootstrap-password {{ admin_password }} \
   --bootstrap-public-url {{ public_endpoint }} \
   --bootstrap-region-id RegionOne
 
-project_test=$(openstack project list \
---os-username admin \
---os-password {{ admin_password }} \
---os-project-name admin \
---os-user-domain-name Default \
---os-project-domain-name Default \
---os-auth-url {{ os_auth_url }} \
---os-identity-api-version 3 \
-| grep service)
+project_test=$(openstack project list | grep service)
 
 if [[ $project_test != '' ]]; then
   echo 'Existing service project detected...skipping creation...'
   echo $project_test
 else
-  openstack project create --domain default --description "Service Project" service \
-  --os-username {{ os_username }} \
-  --os-password {{ os_password }} \
-  --os-project-name {{ os_project_name }} \
-  --os-user-domain-name {{ os_user_domain_name }} \
-  --os-project-domain-name {{ os_project_domain_name }} \
-  --os-auth-url {{ os_auth_url }} \
-  --os-identity-api-version {{ os_identity_api_version }}
+  openstack project create --domain default --description "Service Project" service
 fi
 
-user_role_test=$(openstack role list \
---os-username {{ os_username }} \
---os-password {{ os_password }} \
---os-project-name {{ os_project_name }} \
---os-user-domain-name {{ os_user_domain_name }} \
---os-project-domain-name {{ os_project_domain_name }} \
---os-auth-url {{ os_auth_url }} \
---os-identity-api-version {{ os_identity_api_version }} \
-| grep user)
+user_role_test=$(openstack role list | grep user)
 
 if [[ $user_role_test != '' ]]; then
   echo 'Existing user role detected...skipping creation...'
   echo $user_role_test
 else
-  openstack role create user \
-  --os-username {{ os_username }} \
-  --os-password {{ os_password }} \
-  --os-project-name {{ os_project_name }} \
-  --os-user-domain-name {{ os_user_domain_name }} \
-  --os-project-domain-name {{ os_project_domain_name }} \
-  --os-auth-url {{ os_auth_url }} \
-  --os-identity-api-version {{ os_identity_api_version }}
-fi
+  openstack role create user fi
 
-service_user_test=$(openstack user list \
---os-username {{ os_username }} \
---os-password {{ os_password }} \
---os-project-name {{ os_project_name }} \
---os-user-domain-name {{ os_user_domain_name }} \
---os-project-domain-name {{ os_project_domain_name }} \
---os-auth-url {{ os_auth_url }} \
---os-identity-api-version {{ os_identity_api_version }} \
-| grep keystone)
+service_user_test=$(openstack user list | grep keystone)
 
 if [[ $service_user_test != '' ]]; then
   echo 'Existing keystone service user detected...skipping creation...'
   echo $service_user_test
 else
-openstack user create --domain default --password {{ keystone_service_password }} keystone \
---os-username {{ os_username }} \
---os-password {{ os_password }} \
---os-project-name {{ os_project_name }} \
---os-user-domain-name {{ os_user_domain_name }} \
---os-project-domain-name {{ os_project_domain_name }} \
---os-auth-url {{ os_auth_url }} \
---os-identity-api-version {{ os_identity_api_version }}
-
-openstack role add --project service --user keystone admin \
---os-username {{ os_username }} \
---os-password {{ os_password }} \
---os-project-name {{ os_project_name }} \
---os-user-domain-name {{ os_user_domain_name }} \
---os-project-domain-name {{ os_project_domain_name }} \
---os-auth-url {{ os_auth_url }} \
---os-identity-api-version {{ os_identity_api_version }}
+openstack user create --domain default --password {{ keystone_service_password }} keystone
+openstack role add --project service --user keystone admin 
 fi
 
-ipa_domain_test=$(openstack domain list \
---os-username {{ os_username }} \
---os-password {{ os_password }} \
---os-project-name {{ os_project_name }} \
---os-user-domain-name {{ os_user_domain_name }} \
---os-project-domain-name {{ os_project_domain_name }} \
---os-auth-url {{ os_auth_url }} \
---os-identity-api-version {{ os_identity_api_version }} \
-| grep "IPA Domain")
+ldap_domain_test=$(openstack domain list | grep "LDAP Domain")
 
-if [[ $ipa_domain_test != '' ]]; then
-  echo 'Existing ipa domain detected...skipping creation...'
-  echo $ipa_domain_test
+if [[ $ldap_domain_test != '' ]]; then
+  echo 'Existing ldap domain detected...skipping creation...'
+  echo $ldap_domain_test
 else
-  openstack domain create --description "IPA Domain" ipa \
-  --os-username {{ os_username }} \
-  --os-password {{ os_password }} \
-  --os-project-name {{ os_project_name }} \
-  --os-user-domain-name {{ os_user_domain_name }} \
-  --os-project-domain-name {{ os_project_domain_name }} \
-  --os-auth-url {{ os_auth_url }} \
-  --os-identity-api-version {{ os_identity_api_version }}
+  openstack domain create --description "LDAP Domain" ipa
 fi
