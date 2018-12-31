9b89711bdeeefca55aff8d3736ded0a7bae71681
bitskrieg <bitskrieg@bitskrieg.net>
2018-12-30 22:31:55 -0500

    add keystone internal endpoint to glance

--- formulas/glance/files/mkservice.sh
+++ formulas/glance/files/mkservice.sh
@@ -4,7 +4,7 @@ export OS_PASSWORD={{ admin_password }}
 export OS_PROJECT_NAME=admin
 export OS_USER_DOMAIN_NAME=Default
 export OS_PROJECT_DOMAIN_NAME=Default
-export OS_AUTH_URL={{ internal_endpoint }}
+export OS_AUTH_URL={{ keystone_internal_endpoint }}
 export OS_IDENTITY_API_VERSION=3
 
 service_test=$(openstack service list | grep glance)
