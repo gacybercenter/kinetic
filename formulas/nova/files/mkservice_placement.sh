a8554b0c6bef958e93ba3b833c7ff06a121d60a5
bitskrieg <bitskrieg@bitskrieg.net>
2019-01-01 15:13:41 -0500

    fix typo

--- formulas/nova/files/mkservice_placement.sh
+++ formulas/nova/files/mkservice_placement.sh
@@ -15,7 +15,7 @@ fi
 
 openstack user create --domain default --password {{ placement_service_password }} placement
 openstack role add --project service --user placement admin
-openstack service create --name nova --description "Placement API" placement
+openstack service create --name placement --description "Placement API" placement
 openstack endpoint create --region RegionOne placement public {{ placement_public_endpoint }}
 openstack endpoint create --region RegionOne placement internal {{ placement_internal_endpoint }}
 openstack endpoint create --region RegionOne placement admin {{ placement_admin_endpoint }}
