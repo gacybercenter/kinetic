af68275eb2e07b2ddd55dbd1f26024c314d8f3e1
divey <divey@silotechgroup.com>
2019-01-02 18:59:12 -0500

    fix syntax

--- formulas/glance/files/image_upload.sh
+++ formulas/glance/files/image_upload.sh
@@ -9,12 +9,12 @@ export OS_AUTH_URL={{ internal_endpoint }}
 export OS_IDENTITY_API_VERSION=3
 
 if [ "$(openstack image list | grep {{ glance_name }}) -eq 0" ]; then
-  {% if {{ needs_conversion }} == true %}
+  if {{ needs_conversion }} == true; then
     curl -s -L {{ remote_url }} -o image.qcow2
     qemu-img convert -f qcow2 image.qcow2 {{ glance_name }}.raw
     openstack image create {{ glance_name }}.raw --container-format bare --disk-format raw --public --protected
     rm image.qcow2
-  {% else %}
+  else
     curl -s -L {{ remote_url }} | openstack image create {{ glance_name }} --container-format bare disk-format raw --public --protected
-  {% endif %}
+  fi
 fi
