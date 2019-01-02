51afa50b25abd6f135c5aeedd5226832a9198cde
bitskrieg <bitskrieg@bitskrieg.net>
2019-01-01 19:23:57 -0500

    add initial designate config

---
+++ formulas/designate/files/mkservice.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export OS_USERNAME=admin
+export OS_PASSWORD={{ admin_password }}
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL={{ keystone_internal_endpoint }}
+export OS_IDENTITY_API_VERSION=3
+
+service_test=$(openstack service list | grep designate)
+if [[ $service_test != '' ]]; then
+  echo 'Existing designate service detected...exiting...'
+  exit
+fi
+
+openstack user create --domain default --password {{ designate_service_password }} designate
+openstack role add --project service --user designate admin
+openstack service create --name designate --description "DNS" dns
+openstack endpoint create --region RegionOne dns public {{ designate_public_endpoint }}
+openstack endpoint create --region RegionOne dns internal {{ designate_internal_endpoint }}
+openstack endpoint create --region RegionOne dns admin {{ designate_admin_endpoint }}
+openstack domain create --description "designate projects and users" designate
+openstack user create --domain designate --password {{ designate_service_password }} designate_domain_admin
+openstack role add --domain designate --user-domain designate --user designate_domain_admin admin
---
+++ formulas/designate/install.sls
@@ -0,0 +1,28 @@
+uca:
+  pkgrepo.managed:
+    - humanname: Ubuntu Cloud Archive - Rocky
+    - name: deb http://ubuntu-cloud.archive.canonical.com/ubuntu bionic-updates/rocky main
+    - file: /etc/apt/sources.list.d/cloudarchive-rocky.list
+    - keyid: ECD76E3E
+    - keyserver: keyserver.ubuntu.com
+
+update_packages_uca:
+  pkg.uptodate:
+    - refresh: true
+    - onchanges:
+      - pkgrepo: uca
+    - dist_upgrade: True
+
+designate_packages:
+  pkg.installed:
+    - pkgs:
+      - designate
+      - bind9
+      - bind9utils
+      - bind9-doc
+      - designate-worker
+      - designate-producer
+      - designate-mdns
+      - python-memcache
+      - python-designateclient
+      - python-openstackclient
