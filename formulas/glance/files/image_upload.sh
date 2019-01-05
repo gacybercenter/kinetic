d83f3c1321e3a165e916ff4d0daca06c57ba03d1
divey <divey@silotechgroup.com>
2019-01-04 23:46:06 -0500

    change method for glance image uploads

--- formulas/glance/files/image_upload.sh
+++ formulas/glance/files/image_upload.sh
@@ -10,11 +10,12 @@ export OS_IDENTITY_API_VERSION=3
 
 if [ "$(openstack image list | grep {{ glance_name }}) -eq 0" ]; then
   if {{ needs_conversion }} == true; then
-    curl -s -L {{ remote_url }} -o image.qcow2
+    curl -s -L {{ remote_url }} -o image.qcow2 &&
     qemu-img convert -f qcow2 image.qcow2 {{ glance_name }}.raw
     openstack image create "{{ glance_name }}.raw" --container-format bare --disk-format raw --public --protected
     rm image.qcow2
   else
-    curl -s -L {{ remote_url }} | openstack image create "{{ glance_name }}" --container-format bare --disk-format raw --public --protected
+    #glance image-create-via-import is experimental. Initial testing was successful
+    glance image-create-via-import --import-method web-download --uri {{ remote_url }} --name {{ glance_name }} --container-format bare --disk-format raw --visibility public --protected True
   fi
 fi
