74bdbe23d2c281782c5104f911520a12653016ad
bitskrieg <bitskrieg@bitskrieg.net>
2019-07-05 23:51:21 -0400

    remove stray character

--- formulas/swift/files/mkservice.sh
+++ formulas/swift/files/mkservice.sh
@@ -18,4 +18,4 @@ openstack role add --project service --user swift admin
 openstack service create --name swift --description "OpenStack Object Storage" object-store
 openstack endpoint create --region RegionOne object-store public {{ swift_public_endpoint }}
 openstack endpoint create --region RegionOne object-store internal {{ swift_internal_endpoint }}
-openstack endpoint create --region RegionOne object-store admin {{ swift_admin_endpoint }} 
+openstack endpoint create --region RegionOne object-store admin {{ swift_admin_endpoint }}
