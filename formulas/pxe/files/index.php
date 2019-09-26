93b79b8594eb65a2d601ac18863fd4e8d83d3b3a
bitskrieg <bitskrieg@bitskrieg.net>
2019-09-26 17:59:05 -0400

    adjust index.php given new bootstrap process

--- formulas/pxe/files/index.php
+++ formulas/pxe/files/index.php
@@ -1,18 +1,13 @@
 <?php
 header('Content-type: text/plain');
 $mac = ($_GET["mac"]);
-exec("grep $mac hosts", $output);
-$type_array = explode(" = ", $output[0]);
-$type = $type_array[1];
-exec("echo -n $type-$(uuidgen)", $hostname);
-exec("grep 'd-i netcfg/choose_interface' preseed/$type.preseed | awk '{ print $4 }'", $interface);
+exec("cat assignments/$mac", $hostname);
+$type = explode("-", $hostname[0]);
+exec("grep 'd-i netcfg/choose_interface' preseed/$type[0].preseed | awk '{ print $4 }'", $interface);
 $bootfile = file_get_contents("common.pxe");
-$bootfile = str_replace("http://pxe/preseed/host-type.preseed", "http://".$_SERVER['SERVER_ADDR']."/preseed/".$type.".preseed", $bootfile);
+$bootfile = str_replace("http://pxe/preseed/host-type.preseed", "http://".$_SERVER['SERVER_ADDR']."/preseed/".$type[0].".preseed", $bootfile);
 $bootfile = str_replace("undefined-hostname", "$hostname[0]", $bootfile);
 $bootfile = str_replace("interface=auto", "interface=$interface[0]", $bootfile);
-if (!is_dir('pending_hosts/' . $type)) {
-  mkdir('pending_hosts/' . $type);
-}
-file_put_contents("pending_hosts/$type/$hostname[0]","$hostname[0]");
 echo $bootfile;
+echo $type[0];
 ?>
