a2a31c49cbef730cad90e18e729b802ef6a8864c
bitskrieg <bitskrieg@bitskrieg.net>
2019-09-29 14:58:05 -0400

    increase cephx key length

--- _modules/cephx.py
+++ _modules/cephx.py
@@ -6,7 +6,7 @@ def __virtual__():
     return __virtualname__
 
 def make_key():
-    key = os.urandom(16)
+    key = os.urandom(24)
     header = struct.pack('<hiih', 1, int(time.time()), 0, len(key))
     secret = base64.b64encode(header + key)
     return secret
