82269c77776e76779e24109f7edc6eede3dbba67
bitskrieg <bitskrieg@bitskrieg.net>
2019-09-29 15:00:41 -0400

    add comments

--- _modules/cephx.py
+++ _modules/cephx.py
@@ -1,3 +1,6 @@
+## inspiration for simple salt-key module taken from
+## https://github.com/ceph/ceph-ansible/blob/master/library/ceph_key.py#L26
+
 import os, struct, time, base64
 
 __virtualname__ = 'cephx'
@@ -6,7 +9,7 @@ def __virtual__():
     return __virtualname__
 
 def make_key():
-    key = os.urandom(24)
+    key = os.urandom(16)
     header = struct.pack('<hiih', 1, int(time.time()), 0, len(key))
     secret = base64.b64encode(header + key)
     return secret
