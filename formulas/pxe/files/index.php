6e7afdfc423cbb8cb158f6820ef3a294a462afeb
bitskrieg <bitskrieg@bison.wunsdorf.bitskrieg.net>
2019-11-09 19:57:19 -0500

    drop writing of address and use dns

--- formulas/pxe/files/index.php
+++ formulas/pxe/files/index.php
@@ -7,7 +7,7 @@ $type = explode("-", $host_data[0]);
 if (strpos($host_data[1], 'ubuntu') !== false) {
 exec("grep 'd-i netcfg/choose_interface' preseed/$type[0].preseed | awk '{ print $4 }'", $interface);
 $bootfile = file_get_contents("preseed.pxe");
-$bootfile = str_replace("http://pxe/preseed/host-type.preseed", "http://".$_SERVER['SERVER_ADDR']."/preseed/".$type[0].".preseed", $bootfile);
+$bootfile = str_replace("http://pxe/preseed/host-type.preseed", "http://pxe/preseed/".$type[0].".preseed", $bootfile);
 $bootfile = str_replace("undefined-hostname", "$host_data[0]", $bootfile);
 $bootfile = str_replace("interface=auto", "interface=$interface[0]", $bootfile);
 echo $bootfile;
@@ -16,7 +16,7 @@ echo $bootfile;
 if (strpos($host_data[1], 'centos') !== false) {
 exec("grep 'network --bootproto=dhcp' kickstart/$type[0].kickstart | cut -d'=' -f 3", $interface);
 $bootfile = file_get_contents("kickstart.pxe");
-$bootfile = str_replace("http://pxe/kickstart/host-type.kickstart", "http://".$_SERVER['SERVER_ADDR']."/kickstart/".$type[0].".kickstart", $bootfile);
+$bootfile = str_replace("http://pxe/kickstart/host-type.kickstart", "http://pxe/kickstart/".$type[0].".kickstart", $bootfile);
 $bootfile = str_replace("undefined-hostname", "$host_data[0]", $bootfile);
 $bootfile = str_replace(":undefined-interface", ":$interface[0]", $bootfile);
 echo $bootfile;
