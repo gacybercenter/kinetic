fb5d932ac8d40144159bc4de02a95c30118c9cde
bitskrieg <bitskrieg@bitskrieg.net>
2020-05-24 23:25:06 -0400

    add ubuntu support

--- formulas/pxe/files/index.py
+++ formulas/pxe/files/index.py
@@ -27,6 +27,11 @@ def application (environ, start_response):
             'kernel': "kernel http://mirror.centos.org/centos/7/os/x86_64/images/pxeboot/vmlinuz ks=http://{{ pxe_record }}/kickstart/"+host_type+".kickstart lang=en_US keymap=us ip=::::"+hostname_assignment+":"+interface+":dhcp initrd=initrd.img",
             'initrd': "initrd http://mirror.centos.org/centos/7/os/x86_64/images/pxeboot/initrd.img"
             }
+    elif os_assignment == "ubuntu1804":
+        response_body = body % {
+            'kernel': "kernel http://us.archive.ubuntu.com/ubuntu/dists/bionic-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux --- auto=true url=http://{{ pxe_record }}/preseed/"+host-type+".preseed locale=en_US interface="+interface+"keymap=us netcfg/get_hostname="+hostname_assignment+"netcfg/do_not_use_netplan=true debian-installer/allow_unauthenticated_ssl=true initrd=initrd.gz",
+            'initrd': "initrd http://us.archive.ubuntu.com/ubuntu/dists/bionic-updates/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz"
+            }
 
     response_body = bytes(response_body, encoding= 'utf-8')
     status = '200 OK'
