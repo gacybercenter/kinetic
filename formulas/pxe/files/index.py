96bd567d6c40e4dd2f513613ae23bf021da773f9
bitskrieg <bitskrieg@bitskrieg.net>
2020-05-24 21:40:46 -0400

    testing dynamic kernel generation

--- formulas/pxe/files/index.py
+++ formulas/pxe/files/index.py
@@ -5,7 +5,10 @@ from cgi import parse_qs, escape
 body = """\
 #!ipxe
 
-shell
+%(kernel)s
+%(initrd)s
+
+boot || shell
 """
 
 def application (environ, start_response):
@@ -13,11 +16,16 @@ def application (environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     uuid = d.get('uuid', [''])[0]
     uuid = escape(uuid)
-    os_assignment = open("/var/www/html/assignments/"+uuid.upper(), "r")
-    response_body = body % {
-        'kernel': os_assignment.readline(),
-        'initrd': "Ubuntu"
-        }
+    host_data = open("/var/www/html/assignments/"+uuid.upper(), "r")
+    hostname_assignment = host_data.readline()
+    os_assignment = host_data.readline()
+
+    if os_assignment == centos7:
+        response_body = body % {
+            'kernel': "http://mirror.centos.org/centos/7/os/x86_64/images/pxeboot/vmlinuz ks=http://{{ pxe_record }}/kickstart/host-type.kickstart lang=en_US keymap=us ip=::::undefined-hostname:undefined-interface:dhcp initrd=initrd.img",
+            'initrd': "http://mirror.centos.org/centos/7/os/x86_64/images/pxeboot/initrd.img"
+            }
+
     response_body = bytes(response_body, encoding= 'utf-8')
     status = '200 OK'
     response_headers = [
