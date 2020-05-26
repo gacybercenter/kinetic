0bca04ad873b5768c3af96f5ebd2e44b99a28392
bitskrieg <bitskrieg@bison.wunsdorf.bitskrieg.net>
2020-05-26 15:06:57 -0400

    update address module

--- _modules/address.py
+++ _modules/address.py
@@ -1,8 +1,11 @@
 ## module for IP address management
 ## interfaces with simlpe sqlite3 database in order to obviate the
 ## issue of dhcp relay/calculated addresses
+## includes functions focused on creating leases as well
+## as functions focused on getting those leases issued to clients
+## in a usable format
 
-import sqlite3
+import sqlite3, requests, json
 
 __virtualname__ = 'address'
 
@@ -47,3 +50,21 @@ def release_all_addresses():
     cursor.execute("UPDATE addresses SET host=NULL")
     connection.commit()
     connection.close()
+
+def rest_login(username, password, address):
+    login = requests.post('https://salt:8000/login', verify=False, json={'username':'api',
+                                                                         'password':''+password+',
+                                                                         'eauth':'pam'})
+    token = json.loads(login.text)["return"][0]["token"]
+    return token
+
+def client_get_address(username, password, network, host, address):
+    token = rest_login(username, password, address)
+    lease = requests.post('https://'+address+':8000/', verify=False, headers={'X-Auth-Token':token}, json=[{
+    'client': 'local',
+    'tgt': 'salt',
+    'fun': 'address.get_address',
+    'arg': [network, host]
+    }])
+    address = json.loads(lease.text)["return"][0]["salt"]
+    return address
