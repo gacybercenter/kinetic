4a901fb746f1c6b287ec4dd6ec623d1b02361a8c
bitskrieg <bitskrieg@bitskrieg.net>
2020-06-29 15:34:11 -0400

    fix typo

--- _states/danos.py
+++ _states/danos.py
@@ -80,7 +80,7 @@ def set_resourcegroup(name,
             location = __salt__["danos.make_session"](host, username, password)
             __salt__["danos.delete_configuration"](host, username, password, '/resources/group/'+type+'/'+name, location, **kwargs)
             __salt__["danos.set_configuration"](host, username, password, '/resources/group/'+type+'/'+name, location **kwargs)
-            __salt__["danos.make_session"](host, username, password, location)
+            __salt__["danos.delete_session"](host, username, password, location)
 
             ret["result"] = True
             ret["comment"] = "The "+name+" resource group has been updated"
