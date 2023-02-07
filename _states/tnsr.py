d33a824a4b12d15445a68cdbcc7b8b3ba2643958
mccrusade <mcorulli@augusta.edu>
2023-02-07 21:45:13 +0000

    Chore: Fixed functions and added error checking.

--- _states/tnsr.py
+++ _states/tnsr.py
@@ -15,83 +15,118 @@
 ### Tnsr State Module
 
 import json
+import yaml
 
 __virtualname__ = 'tnsr'
 
 def __virtual__():
     return __virtualname__
 
-def nat_updated(name, new_tables, **kwargs):
-    """
-    Ensure that the NAT config is managed by Salt.
-    """
+def nat_updated(name, 
+                new_tables, 
+                cert, 
+                key, 
+                cacert=False, 
+                **kwargs):
+
+    # Check if "test" is a keyword argument, default to False if not provided
     test = kwargs.get("test", __opts__.get("test", False))
     ret = {"name": name, "changes": {}, "result": True, "comment": ""}
     
-    # Create TNSR session
-    tnsr = __salt__['tnsr.Session'](hostname, cert, key, cacert, headers)
+    # Get current NAT tables
+    current_tables = __salt__["tnsr.nat_tables_request"]("GET", 
+                                                        cert, 
+                                                        key, 
+                                                        cacert=cacert)
 
-    # Get current NAT mapping tables
-    current_tables = json.loads(tnsr.get_nat_mapping_tables())
+    try:
+        # Try to parse current JSON and new YAML data
+        current_tables = json.loads(current_tables)
+        new_tables = yaml.safe_load(new_tables)
+    except Exception as e:
+        ret["result"] = False
+        ret["comment"] = f"Failed to parse the JSON or YAML NAT data. Error: {e}"
+        return ret
 
     # Check if the NAT mapping tables are already managed by Salt
     if current_tables == new_tables:
-        ret["comment"] = "NAT config is already managed by Salt"
+        ret["comment"] = "NAT tables are already managed by Salt"
         return ret
 
     # Check if test mode is enabled
     if test:
         ret["result"] = None
-        ret["comment"] = "NAT config mapping entry would be updated"
+        ret["comment"] = "NAT tables would have been updated"
         return ret
 
     # Update NAT config mapping entry
-    result = tnsr.update_nat_mapping_tables(new_tables)
+    result = __salt__["tnsr.nat_tables_request"]("PUT", 
+                                                cert, 
+                                                key, 
+                                                cacert=cacert, 
+                                                payload=new_tables)
 
     # Check if update was successful
     if not result:
         ret["result"] = False
-        ret["comment"] = "Failed to update NAT config mapping entry"
+        ret["comment"] = "Failed to update NAT tables"
         return ret
 
     ret["changes"]["updated"] = result
-    ret["comment"] = "Successfully updated NAT config mapping entry"
+    ret["comment"] = "Successfully updated NAT tables"
     return ret
 
 
-def unbound_updated(name, new_zones, **kwargs):
-    """
-    Ensure that the DNS config is managed by Salt.
-    """
+def unbound_updated(name, 
+                    new_zones, 
+                    cert, 
+                    key, 
+                    cacert=False, 
+                    **kwargs):
+
+    # Check if "test" is a keyword argument, default to False if not provided
     test = kwargs.get("test", __opts__.get("test", False))
     ret = {"name": name, "changes": {}, "result": True, "comment": ""}
 
-    # Create TNSR session
-    tnsr = __salt__["tnsr.Session"](hostname, cert, key, cacert, headers)
-
     # Get current Unbound zones
-    current_zones = json.loads(tnsr.get_unbound_local_zones())
+    current_zones = __salt__["tnsr.unbound_zones_request"]("GET", 
+                                                            cert, 
+                                                            key, 
+                                                            cacert=cacert)
+    
+    try:
+        # Try to parse current JSON and new YAML data
+        current_zones = json.loads(current_zones)
+        new_zones = yaml.safe_load(new_zones)
+    except Exception as e:
+        ret["result"] = False
+        ret["comment"] = f"Failed to parse the JSON or YAML DNS data. Error: {e}"
+        return ret
 
     # Check if the Unbound zones are already managed by Salt
     if current_zones == new_zones:
-        ret["comment"] = "DNS config is already managed by Salt"
+        ret["comment"] = "DNS mapping is already managed by Salt"
         return ret
 
     # Check if test mode is enabled
     if test:
         ret["result"] = None
-        ret["comment"] = "DNS config mapping entry would be updated"
+        ret["comment"] = "DNS mapping would have been updated"
         return ret
 
     # Update Unbound zones
-    result = tnsr.update_unbound_local_zones(new_zones)
+    result = __salt__["tnsr.unbound_zones_request"]("PUT", 
+                                                    cert, 
+                                                    key, 
+                                                    cacert=cacert, 
+                                                    payload=new_zones)
 
     # Check if update was successful
     if not result:
         ret["result"] = False
-        ret["comment"] = "Failed to update DNS config mapping entry"
+        ret["comment"] = "Failed to update DNS mapping"
         return ret
     
     ret["changes"]["updated"] = result
-    ret["comment"] = "Successfully updated NAT config mapping entry"
+    ret["comment"] = "Successfully updated DNS mapping"
     return ret
