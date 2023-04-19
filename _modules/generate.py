b944d6f79b315a3fb6894197825d3fb13bc86e20
djivey <davivey@augusta.edu>
2023-04-18 21:07:12 -0400

    CHORE: adjust format

--- _modules/generate.py
+++ _modules/generate.py
@@ -29,10 +29,10 @@ def __virtual__():
     return __virtualname__
 
 def mac(prefix='52:54:00'):
-   return '{0}:{1:02X}:{2:02X}:{3:02X}'.format(prefix,
-                                               random.randint(0, 0xff),
-                                               random.randint(0, 0xff),
-                                               random.randint(0, 0xff))
+    return '{0}:{1:02X}:{2:02X}:{3:02X}'.format(prefix,
+                                              random.randint(0, 0xff),
+                                              random.randint(0, 0xff),
+                                              random.randint(0, 0xff))
 
 def erlang_cookie(length = 20):
     return ''.join(random.choice(string.ascii_uppercase) for i in range(length))
@@ -42,4 +42,3 @@ def cephx_key():
     header = struct.pack('<hiih', 1, int(time.time()), 0, len(key))
     secret = base64.b64encode(header + key)
     return secret.decode('utf-8')
-
