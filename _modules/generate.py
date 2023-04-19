b487fe9c1067464e0d7a802637c33fbfd0f5795e
djivey <davivey@augusta.edu>
2023-04-18 20:59:10 -0400

    FIX: rewrite mac function in generate.py to fix random mac format errors

--- _modules/generate.py
+++ _modules/generate.py
@@ -29,7 +29,10 @@ def __virtual__():
     return __virtualname__
 
 def mac(prefix='52:54:00'):
-    return f'{prefix}'.join(f':{random.randint(0, 0xff):02x}' for i in range(3))
+   return '{0}:{1:02X}:{2:02X}:{3:02X}'.format(prefix,
+                                               random.randint(0, 0xff),
+                                               random.randint(0, 0xff),
+                                               random.randint(0, 0xff))
 
 def erlang_cookie(length = 20):
     return ''.join(random.choice(string.ascii_uppercase) for i in range(length))
@@ -39,3 +42,4 @@ def cephx_key():
     header = struct.pack('<hiih', 1, int(time.time()), 0, len(key))
     secret = base64.b64encode(header + key)
     return secret.decode('utf-8')
+
