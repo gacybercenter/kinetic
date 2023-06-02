e078d834d8772e01ec6c3068d2c751ec734f69e1
Jonathan Race <jrace@augusta.edu>
2023-06-02 08:03:53 -0400

    fix: replace kernel files everytime to ensure current with ISO

--- formulas/pxe/files/kernel-extract.sh
+++ formulas/pxe/files/kernel-extract.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -f /srv/tftp/jammy/vmlinuz /srv/tftp/jammy/initrd
 mount /srv/tftp/jammy/ubuntu2204.iso /mnt/
 cp /mnt/casper/vmlinuz /srv/tftp/jammy/vmlinuz
 cp /mnt/casper/initrd /srv/tftp/jammy/initrd
