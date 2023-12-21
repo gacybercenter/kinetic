906f6431791708051837ca87caafb7acce148f9d
Jonathan Race <jrace@augusta.edu>
2023-12-21 14:54:57 -0500

    fix: system scop test for horizon

--- formulas/horizon/files/local_settings.py
+++ formulas/horizon/files/local_settings.py
@@ -295,4 +295,5 @@ USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 DESIGNATE = { 'records_use_fips': True }
 SESSION_TIMEOUT = {{ session_timeout }}
+SYSTEM_SCOPE_SERVICES = []
 {{ theming }}
