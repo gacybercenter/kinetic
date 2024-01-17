4899014dd78881c2623a6d42f00baaa85e8f1801
Jonathan Race <jrace@augusta.edu>
2024-01-17 08:52:34 -0500

    chore: cleanup and stage address.py for future work and examples

--- _modules/address.py
+++ _modules/address.py
@@ -21,113 +21,56 @@
 
 
 import json
+import logging
 import os
 import requests
 import sqlite3
-import time
+
+log = logging.getLogger(__name__)
 
 __virtualname__ = 'address'
 
 def __virtual__():
     return __virtualname__
 
-def login(database = '/srv/addresses/addresses.db'):
-    connection = sqlite3.connect(database)
-    return connection
-
-def get_address(network, host):
-    connection = login()
-    cursor = connection.cursor()
-    query_values = (host, network)
-    cursor.execute('SELECT address FROM addresses where host=? AND network=?', query_values)
-    existing_lease = cursor.fetchone()
-    if existing_lease is None:
-        network_value = (network, )
-        cursor.execute('SELECT address FROM addresses where host IS NULL AND network=?', network_value)
-        address = cursor.fetchone()[0]
-
-        host_address_value = (host, address)
-        cursor.execute("UPDATE addresses SET host=? WHERE address=?", host_address_value)
-        connection.commit()
-        connection.close()
-        return address
-    return existing_lease[0]
-
-
-def release_single_address(address):
-    connection = login()
-    cursor = connection.cursor()
-    address_value = (address, )
-    cursor.execute("UPDATE addresses SET host=NULL WHERE address=?", address_value)
-    connection.commit()
-    connection.close()
-
-def expire_dead_hosts():
-    connection = login()
-    cursor = connection.cursor()
-    cursor.execute("SELECT host FROM addresses WHERE host IS NOT NULL")
-    all_leases = cursor.fetchall()
-    working_list = all_leases
-    minions = os.listdir('/etc/salt/pki/master/minions')
-    for minion in minions:
-        minion_value = (minion, )
-        cursor.execute("SELECT host FROM addresses WHERE host=?", minion_value)
-        minion_leases = cursor.fetchall()
-        working_list = [i for i in working_list if i not in minion_leases]
-    working_list = list(set(working_list))
-    for host in working_list:
-        release_all_host_addresses(str(host[0]))
-    cursor.execute("SELECT host FROM addresses WHERE host IS NOT NULL")
-    remaining_leases = cursor.fetchall()
-    connection.commit()
-    connection.close()
-    return remaining_leases
-
-def release_all_host_addresses(host):
-    connection = login()
-    cursor = connection.cursor()
-    host_value = (host, )
-    cursor.execute("UPDATE addresses SET host=NULL WHERE host=?", host_value)
-    connection.commit()
-    connection.close()
-
-def release_all_addresses():
-    connection = login()
-    cursor = connection.cursor()
-    cursor.execute("UPDATE addresses SET host=NULL")
-    connection.commit()
-    connection.close()
-
 def rest_login(username, password, url):
-    login = requests.post(
-                f'https://{url}:8000/login',
-                verify=False,
-                json={
-                    'username':username,
-                    'password':password,
-                    'eauth':'pam'
-                }
-            )
-    token = json.loads(login.text)["return"][0]["token"]
-    return token
+    try:
+        login = requests.post(
+                    f'https://{url}:8000/login',
+                    verify=False,
+                    json={
+                        'username':username,
+                        'password':password,
+                        'eauth':'pam'
+                    }
+                )
+        token = json.loads(login.text)["return"][0]["token"]
+        return token
+    except Exception as e:
+        log.error("Unable to authenticate foruse %s: %s", username, e)
+        return False
 
 # NOTE(chateaulav): this function needs to be refactored to be single focused
 def client_get_address(username, password, network, host, url, target):
-    token = rest_login(username, password, url)
-    lease = requests.post(
-                f'https://{url}:8000/',
-                verify=False,
-                headers={
-                    'X-Auth-Token':token
-                },
-                json=[
-                    {
-                    'client': 'local',
-                    'tgt': target,
-                    'fun': 'address.get_address',
-                    'arg': [network, host]
-                    }
-                ]
-            )
-    address = json.loads(lease.text)["return"][0][target]
-    return address
+    try:
+        token = rest_login(username, password, url)
+        lease = requests.post(
+                    f'https://{url}:8000/',
+                    verify=False,
+                    headers={
+                        'X-Auth-Token':token
+                    },
+                    json=[
+                        {
+                        'client': 'local',
+                        'tgt': target,
+                        'fun': 'address.get_address',
+                        'arg': [network, host]
+                        }
+                    ]
+                )
+        address = json.loads(lease.text)["return"][0][target]
+        return address
+    except Exception as e:
+        log.error("Unable to execute: %s", e)
+        return False
