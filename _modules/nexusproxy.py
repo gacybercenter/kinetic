29dbec6061d3d3f8851d665aa5b3462d57d7687f
Jonathan Race <jrace@augusta.edu>
2024-02-19 01:03:17 -0500

    feat: introduction of nexusproxy state and modules to support updating user password, and managing repositories

---
+++ _modules/nexusproxy.py
@@ -0,0 +1,247 @@
+## Copyright 2020 Augusta University
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##    http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## nexusproxy module - primarily used for provisioning repositories
+## could potentially be fleshed out and become formal fully-featured
+## salt module
+
+import socket
+import requests
+import urllib3
+import json
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+socket.setdefaulttimeout(0.5)
+
+__virtualname__ = "nexusproxy"
+
+def __virtual__():
+    return __virtualname__
+    
+
+def list_users(host: str,
+               port: str,
+               username: str,
+               password: str,
+               ):
+    '''
+    This function is used to list all repositories in Nexus Proxy.
+    @param host: Nexus host ip or dns name to inlude the http:// or https://
+    @param port: Nexus listening port
+    @param username: Nexus username
+    @param password: Nexus password
+    '''
+    response = json.dumps(requests.get(f"{host}:{port}/service/rest/v1/security/users",
+                            auth=(username, password),
+                            verify=False).json(), indent=4)
+    return response
+
+def change_user_password(host: str,
+                         port: str,
+                         username: str,
+                         password: str,
+                         user: str,
+                         new_password: str,
+                         ):
+    '''
+    This function is used to change a user's password in Nexus Proxy.
+    @param host: Nexus host ip or dns name to inlude the http:// or https://
+    @param port: Nexus listening port
+    @param username: Nexus username
+    @param password: Nexus password
+    @param user: User to change password for
+    @param new_password: New password for user
+    '''
+    response = requests.put(f"{host}:{port}/service/rest/v1/security/users/{user}/change-password",
+                            auth=(username, password),
+                            headers={"Content-Type": "text/plain"},
+                            data=new_password,
+                            verify=False)
+    return response.status_code
+
+def list_repositories(host: str,
+                      port: str,
+                      username: str,
+                      password: str,
+                      ):
+    '''
+    This function is used to list all repositories in Nexus Proxy.
+    @param host: Nexus host ip or dns name to inlude the http:// or https://
+    @param port: Nexus listening port
+    @param username: Nexus username
+    @param password: Nexus password
+    '''
+    response = json.dumps(requests.get(f"{host}:{port}/service/rest/v1/repositories",
+                            auth=(username, password),
+                            verify=False).json(), indent=4)
+    return response
+
+def list_repository(host: str,
+                    port: str,
+                    username: str,
+                    password: str,
+                    name: str,
+                    ):
+    '''
+    This function is used to list a specific repository in Nexus Proxy.
+    @param host: Nexus host ip or dns name to inlude the http:// or https://
+    @param port: Nexus listening port
+    @param username: Nexus username
+    @param password: Nexus password
+    @param name: Repository name
+    expected return:
+    {
+      "name" : "debian",
+      "format" : "apt",
+      "type" : "proxy",
+      "url" : "http://10.200.1.173:3142/repository/debian",
+      "attributes" : {
+        "proxy" : {
+          "remoteUrl" : "http://deb.debian.org/debian/"
+        }
+      }
+    }
+    '''
+    response = requests.get(f"{host}:{port}/service/rest/v1/repositories/{name}",
+                            auth=(username, password),
+                            verify=False)
+    if response.status_code == 200:
+        response = json.dumps(response.json(), indent=4)
+        return response
+    return response.status_code
+
+def delete_repository(host: str,
+                      port: str,
+                      username: str,
+                      password: str,
+                      name: str,
+                      ):
+    '''
+    This function is used to delete a specific repository in Nexus Proxy.
+    @param host: Nexus host ip or dns name to inlude the http:// or https://
+    @param port: Nexus listening port
+    @param username: Nexus username
+    @param password: Nexus password
+    @param name: Repository name
+    '''
+    response = json.dumps(requests.delete(f"{host}:{port}/service/rest/v1/repositories/{name}",
+                            auth=(username, password),
+                            verify=False).json(), indent=4)
+    return response
+
+def add_proxy_repository(host: str,
+                         port: str,
+                         username: str,
+                         password: str,
+                         name: str,
+                         repoType: str,
+                         remoteUrl: str,
+                         **kwargs
+                         ):
+    '''
+    This function is used to add a proxy repository to Nexus Proxy, and currently
+    only supports apt, yum, and docker repositories.
+    @param host: Nexus host ip or dns name to inlude the http:// or https://
+    @param port: Nexus listening port
+    @param username: Nexus username
+    @param password: Nexus password
+    @param name: Repository name
+    @param repoType: Repository type
+    @param remoteUrl: Remote URL of repository
+    @param kwargs: Additional parameters for repository using appropriate dict/list structures
+    '''
+    payload = {
+                "name": name,
+                "online": True,
+                "storage": {
+                    "blobStoreName": "default",
+                    "strictContentTypeValidation": True
+                    },
+                "cleanup": {
+                    "policyNames": [
+                        "string"
+                        ]
+                        },
+                "proxy": {
+                    "remoteUrl": remoteUrl,
+                    "contentMaxAge": 1440,
+                    "metadataMaxAge": 1440
+                    },
+                "negativeCache": {
+                    "enabled": True,
+                    "timeToLive": 1440
+                    },
+                "httpClient": {
+                    "blocked": False,
+                    "autoBlock": True,
+                    "connection": {
+                        "retries": 0,
+                        "userAgentSuffix": "string",
+                        "timeout": 60,
+                        "enableCircularRedirects": False,
+                        "enableCookies": False,
+                        "useTrustStore": False
+                        },
+                    "authentication": {
+                        "type": "username",
+                        "username": "string",
+                        "password": "string",
+                        "ntlmHost": "string",
+                        "ntlmDomain": "string"
+                        }
+                    },
+                "routingRule": "string",
+                "replication": {
+                    "preemptivePullEnabled": False,
+                    "assetPathRegex": "string"
+                    }
+                }
+    if repoType == "apt":
+        payload.update({
+                    "apt": {
+                        "distribution": "*",
+                        "flat": False
+                        }
+                    })
+    elif repoType == "yum":
+        payload.update({
+            "yumSigning": {
+                "keypair": "string",
+                "passphrase": "string"
+                }
+            })
+    elif repoType == "docker":
+        payload.update({
+            "docker": { 
+                "v1Enabled": False,
+                "forceBasicAuth": True,
+                "httpPort": 8082,
+                "httpsPort": 8083,
+                "subdomain": "docker-a"
+                },
+            "dockerProxy": {
+                "indexType": "HUB",
+                "indexUrl": "string",
+                "cacheForeignLayers": True,
+                "foreignLayerUrlWhitelist": [
+                    "string"
+                    ]
+                }
+            })
+    payload.update(kwargs)
+    response = requests.post(f"{host}:{port}/service/rest/v1/repositories/{repoType}/proxy",
+                            auth=(username, password),
+                            headers={"Content-Type": "application/json"},
+                            json=payload,
+                            verify=False)
+    return response.status_code
---
+++ _states/nexusproxy.py
@@ -0,0 +1,113 @@
+### Guacamole State Module
+"""Guacamole is State Module"""
+import json
+
+__virtualname__ = "guacamole"
+
+def __virtual__():
+    return __virtualname__
+
+def update_user_password(name, host, port, username, password, user, new_password, **kwargs):
+    '''
+    This function is used to change a user password in Nexus Proxy.
+    
+    @param host: Nexus host ip or dns name to inlude the http:// or https://
+    @param port: Nexus listening port
+    @param username: Nexus username
+    @param password: Nexus password
+    @param user: User to change password for
+    @param new_password: New password for user
+    ''' 
+    ret = {"name": name, "result": False, "changes": {}, "comment": ""}
+    if "test" not in kwargs:
+        kwargs["test"] = __opts__.get("test", False)
+    if __opts__["test"] == True:
+        ret["comment"] = f'The state of "{name}" will be changed.'
+        ret["changes"] = {
+            "old": "",
+            "new": f'new password: {new_password} would be set'
+        }
+        ret["result"] = None
+        return ret
+    current_users = json.loads(__salt__["nexusproxy.list_users"](host,
+                                                                 port,
+                                                                 username,
+                                                                 password))
+    userList = []
+    for user in current_users:
+        userList += user['userId']
+    if user not in userList:
+        ret["comment"] = f'User: "{user}" is not present.'
+        ret["result"] = False
+        return ret
+    new_state = json.loads(__salt__["nexusproxy.change_user_password"](host,
+                                                                       port,
+                                                                       username,
+                                                                       password,
+                                                                       user,
+                                                                       new_password))
+    if new_state == 204:
+        ret["comment"] = f'The state of "{name}" was changed successfully!'
+        ret["changes"] = {
+            "old": '',
+            "new": f'new password: {new_password} | http status code: {new_state}'
+        }
+        ret["result"] = True
+        return ret
+    ret["comment"] = f'The state of "{name}" returned status code: {new_state}'
+    ret["result"] = False
+    return ret
+
+def add_proxy_repository(name, host, port, username, password, repoType, remoteUrl, test: bool = False, **kwargs):
+    '''
+    This function is used to add a proxy repository in Nexus Proxy.
+    
+    @param host: Nexus host ip or dns name to inlude the http:// or https://
+    @param port: Nexus listening port
+    @param username: Nexus username
+    @param password: Nexus password
+    @param test: Boolean to test the state
+    @param kwargs: Dictionary of repository parameters
+    '''
+    ret = {"name": name, "result": False, "changes": {}, "comment": ""}
+    current_state = json.loads(__salt__["nexusproxy.list_repository"](host,
+                                                                        port,
+                                                                        username,
+                                                                        password,
+                                                                        name))['name']
+    if test == True:
+        ret["comment"] = f'The state of "{name}" will be changed.'
+        ret["changes"] = {
+            "old": current_state,
+            "new": f'new repository: {name} would be added',
+        }
+        ret["result"] = None
+        return ret
+    if current_state == name:
+        ret["comment"] = f'Repository: "{name}" is already present.'
+        ret["result"] = True
+        return ret
+    new_state = __salt__["nexusproxy.add_proxy_repository"](host,
+                                                                       port,
+                                                                       username,
+                                                                       password,
+                                                                       name,
+                                                                       repoType,
+                                                                       remoteUrl,
+                                                                       **kwargs)
+    if new_state == 201:
+        repository = json.loads(__salt__["nexusproxy.list_repository"](host,
+                                                                        port,
+                                                                        username,
+                                                                        password,
+                                                                        name))
+        ret["comment"] = f'Repository: "{name}" was added successfully!'
+        ret["changes"] = {
+            "old": '',
+            "new": repository
+        }
+        ret["result"] = True
+        return ret
+    ret["comment"] = f'The state of "{name}" returned status code: {new_state}'
+    ret["result"] = False
+    return ret
