a675ddbf334156e5dc4b10a903fe87303720b2d3
Mark Danielson <mdanielson@augusta.edu>
2024-10-28 07:17:09 -0400

    json

--- _states/nexusproxy.py
+++ _states/nexusproxy.py
@@ -67,10 +67,10 @@ def activate_realms(name, realms, host, port, username, password):
     @param password: Nexus password
     '''
     ret = {"name": name, "realms": realms, "result": False, "changes": {}, "comment": ""}
-    current_state = json.dumps(__salt__["nexusproxy.list_active_realms"](host,
+    current_state = __salt__["nexusproxy.list_active_realms"](host,
                                                                          port,
                                                                          username,
-                                                                         password))
+                                                                         password)
     if current_state == realms:
         ret["comment"] = f'Realms: "{current_state}" is already set'
         ret["result"] = True
