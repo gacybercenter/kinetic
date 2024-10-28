6f892847bb537e908e5df3514bae4e77c659f835
Mark Danielson <mdanielson@augusta.edu>
2024-10-28 08:34:01 -0400

    set

--- _states/nexusproxy.py
+++ _states/nexusproxy.py
@@ -71,7 +71,7 @@ def activate_realms(name, realms, host, port, username, password):
                                                                          port,
                                                                          username,
                                                                          password)
-    if current_state == realms:
+    if set(current_state) == set(realms):
         ret["comment"] = f'Realms: "{current_state}" is already set'
         ret["result"] = True
         return ret
