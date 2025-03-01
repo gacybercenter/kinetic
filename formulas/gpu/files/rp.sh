f597011af147a2cb8032bc7436f08ab94647a5c7
Mark Danielson <mdanielson@augusta.edu>
2025-03-01 16:32:58 -0500

    added package

--- formulas/gpu/files/rp.sh
+++ formulas/gpu/files/rp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+wget http://cache.dev.gacyberrange.org:3142/repository/nvidia-550/nvidia-vgpu-ubuntu-aie-550_550.144.02_amd64.deb
+dpkg -i nvidia-vgpu-ubuntu-aie-550_550.144.02_amd64.deb
 
 cloud="--os-cloud kinetic"
 for i in $(openstack $cloud resource provider list -c uuid -c name -f value|grep _e1_ | awk '{print $1}'); do openstack $cloud resource provider trait set --trait CUSTOM_NVIDIA_20B5_A100D_80C $i; done
