b894183509b6900e14a4b0b4a965a6d19deda582
Mark Danielson <mdanielson@augusta.edu>
2025-07-02 11:46:18 -0400

    setup ipmi password

--- _modules/kinetic-k8s.py
+++ _modules/kinetic-k8s.py
@@ -780,7 +780,7 @@ def userdata_present(namespace, bmh_name, pillar_data, userdata_template_path='s
             'message': f"An error occurred during userdata_present operation: {str(e)}"
         }
 
-def bmc_auth_present(namespace, ipmi_password, secret_name='bmc-auth', bmc_auth_template_path='salt://formulas/bmo/files/bmc-auth.j2'):
+def bmc_auth_present(namespace, ipmi, secret_name='bmc-auth', bmc_auth_template_path='salt://formulas/bmo/files/bmc-auth.j2'):
     """
     Ensure that the bmc-auth Secret in Kubernetes matches the desired state defined by pillar data
     and Jinja2 template. Creates the Secret if it doesn't exist, or updates it if it differs.
@@ -839,7 +839,7 @@ def bmc_auth_present(namespace, ipmi_password, secret_name='bmc-auth', bmc_auth_
             bmc_auth_context = {
                 'pillar': {
                     'bmo_namespace': namespace,
-                    'ipmi_password': ipmi-password
+                    'ipmi_password': ipmi,
                 }
             }
 
