6254b536de24fedbf3c52a1378e79c3bbba88637
Mark Danielson <mdanielson@augusta.edu>
2025-07-03 09:48:26 -0400

    debug

--- _modules/kinetic-k8s.py
+++ _modules/kinetic-k8s.py
@@ -509,6 +509,8 @@ def networkdata_present(namespace, bmh_name, pillar_data, network_template_path=
                 dhcp_options = {}
                 management_subnet = None
                 networking_data = full_pillar.get('networking', {})
+                if not networking_data:
+                    print("network data": network_data)
                 dhcp_options = full_pillar.get('dhcp-options', {})
                 management_subnet = networking_data.get('subnets',{}).get('management')
                 
