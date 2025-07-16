4b22a51859d3b1e786173ac5fd45a13bcc517500
Mark Danielson <mdanielson@augusta.edu>
2025-07-16 14:09:22 -0400

    salt pillar

--- _states/k8s.py
+++ _states/k8s.py
@@ -66,7 +66,7 @@ def uuids_present(name, namespace, secret_name, pillar_data=None, deployment_nam
         if pillar_data is None:
             if pillar_key is None:
                 raise SaltInvocationError('Either pillar_data or pillar_key must be provided.')
-            pillar_data = __salt__['pillar.get'](salt-master:uuids, {})
+            pillar_data = __salt__['pillar.get'](salt-master.uuids, {})
 
         # Call the execution module function
         result = __salt__['kinetic-k8s.uuids_secret_present'](namespace, secret_name, pillar_data, deployment_name, wait_timeout, wait_interval)
