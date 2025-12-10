a98a53d18dd50709e3921c707000f472dc8cf260
Mark Danielson <mdanielson@augusta.edu>
2025-12-10 14:27:52 -0500

    admin

--- _modules/kinetic-k8s.py
+++ _modules/kinetic-k8s.py
@@ -4022,7 +4022,7 @@ def keycloak_cluster_present(namespace, name, hostname, start_optimized=False, i
             "image": image,
             "hostname": {
                 "hostname": 'https://' + hostname,  # Nested hostname as per Keycloak Operator documentation
-                "admin": 'https://admin' + name + 'rsc.gacyberrange.org',
+                "admin": 'https://admin.' + name + '.rsc.gacyberrange.org',
                 "backchannelDynamic": True
             },
             "db": {
