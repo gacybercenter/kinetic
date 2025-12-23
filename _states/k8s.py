5a9edbf16eee6ec33fd10e3daa74d2a4ec84cbe3
Mark Danielson <mdanielson@augusta.edu>
2025-12-23 07:47:22 -0500

    Format error message for Certificate failure with newline

--- _states/k8s.py
+++ _states/k8s.py
@@ -2180,7 +2180,7 @@ def certmanager_certificate_present(
             "renew_before": renew_before,
         }
         ret["comment"] = (
-            f"Failed to ensure Certificate {certificate_name} in namespace {namespace}: {str(e)[:100]} {error_details}"
+            f"Failed to ensure Certificate {certificate_name} in namespace {namespace} \n {error_details}: {str(e)[:100]}"
         )
         ret["changes"] = {}
 
