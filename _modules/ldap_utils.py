52f01e545a4f8bb17b01692a74de1f29c1706774
Mark Danielson <mdanielson@augusta.edu>
2026-03-11 14:28:03 -0400

    Remove redundant comment about SCOPE_BASE in dn_exists

--- _modules/ldap_utils.py
+++ _modules/ldap_utils.py
@@ -982,13 +982,12 @@ def dn_exists(spec_name, dn, desired_attributes=None):
             return ret
 
         conn = conn_result["conn"]
-        # Use SCOPE_BASE to check exactly the specified DN
-        dn=
+
         attr_list = (
             ["dn"] + list(desired_attributes.keys()) if desired_attributes else ["dn"]
         )
         # Split the DN to get the first element (RDN, e.g., 'ou=users' from 'ou=users,dc=example,dc=com')
-        dn_parts = dn.split(',', 1)  # Split only on the first comma
+        dn_parts = dn.split(",", 1)  # Split only on the first comma
         first_element = dn_parts[0]  # e.g., 'ou=users'
 
         result = conn.search_s(
