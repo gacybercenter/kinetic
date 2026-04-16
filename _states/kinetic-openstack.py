1e59d4182ee1524b96e8191bf5334fedd9263dda
Mark Danielson <mdanielson@augusta.edu>
2026-04-16 14:21:34 -0400

    Update virtual check to use get_projects in kinetic-openstack

--- _states/kinetic-openstack.py
+++ _states/kinetic-openstack.py
@@ -46,7 +46,7 @@ def __virtual__():
     """
     Only load if the openstack module is available in __salt__
     """
-    if "kinetic-openstack.project_present" in __salt__:
+    if "kinetic-openstack.get_projects" in __salt__:
         return __virtualname__
     return False
 
